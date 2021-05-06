6e96c20ffe9fa55b7f75ec5973a1a09fe8fef265
zhanglikun <iuxt@qq.com>
2021-05-06 09:53:30 +0800

    add windows mount drive

---
+++ mount_webdav/windows盘符映射.bat
@@ -0,0 +1,15 @@
+@echo off
+
+choice /t 0 /d y /n >nul
+
+setlocal enabledelayedexpansion
+for /f "delims=" %%i in ('type "config.ini"^| find /i "="') do set %%i
+echo %url%
+echo %username%
+
+REM 映射到Z盘, PERSISTENT表示是否记忆
+net use Z: %url% /user:%username% %password% /PERSISTENT:no
+
+REM 这个是将一个目录映射成一个盘, 不过有些软件不兼容.
+subst D: C:\Files
+
