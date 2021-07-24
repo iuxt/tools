fdb7cb62139069c597ebb7525e256c0c717bb05e
zhanglikun <iuxt@qq.com>
2021-07-24 21:29:01 +0800

    add beyond compare 重置试用脚本

---
+++ beyond_compare重置试用.py
@@ -0,0 +1,22 @@
+# 这个脚本是重置beyondcompare试用的脚本，就是可以实现一直试用。
+import os
+import getpass
+
+username = getpass.getuser()
+
+def get_sid():
+    output = os.popen("whoami /user").readlines()
+    for line in output:
+        if username in line:
+            sid = line.split(" ")[-1].strip("\n")
+    return sid
+
+
+def delete_bc_trial_information():
+    sid = get_sid()
+    result = os.popen("reg delete \"HKEY_USERS\%s\Software\Scooter Software\Beyond Compare 4\" /v CacheId /f") % sid
+    return result
+
+if __name__ == '__main__':
+    print(delete_bc_trial_information())
+    os.system("pause")
